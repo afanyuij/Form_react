a20bd7bc68c9d91495a7e37ab3a8e714597e0ff3
afanyuij <afanyuij@gmail.com>
2025-04-03 20:26:12 +0200

    done with work

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
+import { useState } from "react"
+import { Login } from "./components/Login"
+import { Register } from "./components/Register"
 
 export default function App(){
+  const [active, setActive]=useState("login")
   return (
-    <div className="gif-bg h-screen w-full flex items-center justify-center text-white text-2xl ">
+    <div className="gif-bg h-screen w-ful text-white text-2xl ">
+      <nav className="flex justify-between items-center p-2 px-12 bg-gradient-to-r from-cyan-800/80 via-indigo-800/70  to-fuchsia-900/80 shadow-lg backdrop-blur-md ">
+      <div className="flex justify-center items-center">
+  
+      <img src="/public/icons8-playerme-100.png" className="w-[50px]" alt=""  />
+        <h1 className="text-2xl font-bold bg-gradient-to-r from-gray-100 via-fuchsia-300 to-yellow-400 text-transparent bg-clip-text pop  text-center">Universe</h1>
+  </div>
+  <div className="flex justify-center items-center gap-6">
+  <button onClick={()=>setActive("login")} className="bg-fuchsia-500 bg-gradient-to-r from-indigo-500/80 via-fuchsia-200/40 to-fuchsia-700/70 backdrop-blur-md shadow-lg  hover:scale-[1.1] cursor-pointer duration-300 text-white text-[17px] pop w-[50%] p-2 rounded-md ">SignIn</button>
 
-      <div className="pop w-[60%] ">
-        <h1 className="text-7xl text-center">Welcome back user</h1>
+  <button onClick={()=>setActive("register")} className="bg-fuchsia-500 bg-gradient-to-r from-indigo-500/80 via-fuchsia-200/40 to-fuchsia-700/70 backdrop-blur-md shadow-lg  hover:scale-[1.1] cursor-pointer duration-300 text-white text-[17px] pop w-[50%] p-2 rounded-md ">SignUp</button>
 
-      </div>
-     <div className="flex justify-left w-[40%] ">
-
-      <form className="flex flex-col gap-4 w-[60%] bg-fuchsia-900/80 rounded-4xl  shadow-2xl border-none p-10
-      backdrop-blur-md bg-gradient-to-r from-indigo-900/80 via-fuchsia-700/50 to-fuchsia-950/80 border border-white/20
-      ">
-        <h1 className="text-4xl font-extrabold block text-center mt-3 mb-6 tracking-wider">Sign<span className="text-fuchsia-100 pop">In</span></h1>
-        
-        <div className="flex w-full mb-4 justify-center">
-        
-        <img src="/public/icons8-user-100 (1).png" className="w-[100px] " alt="" srcset="" />
-
-        </div>
-        <input type="text" placeholder="Enter your Email" className="p-3 text-[18px] pop rounded-4xl bg-gradient-to-r from-fuchsia-600/40 via-fushsia-400/50 to-indigo-600/30  border border-white/20 backdrop-blur-md shadow-lg  mb-2 bg-fuchsia-800/80 text-white outline-none " />
-        <input type="password" placeholder="Enter your Password" className="text-[18px] pop outline-none p-3  mb-2 bg-fuchsia-800/80 text-white rounded-4xl bg-gradient-to-r from-fuchsia-600/40 via-fushsia-400/50 to-indigo-600/30  border border-white/20 backdrop-blur-md shadow-lg" />
-        <div className="flex w-full justify-center mb-3">
-
-        <button className="bg-fuchsia-500 bg-gradient-to-r from-indigo-500/80 via-fuchsia-200/40 to-fuchsia-700/70 backdrop-blur-md shadow-lg border border-white/20 hover:scale-[1.1] cursor-pointer duration-300 text-white  pop w-[50%] p-2 rounded-2xl ">Login</button>
-        </div>
-        <p className="text-white text-sm text-center">Don't have an account <span className="text-fuchsia-100 underline cursor-pointer text-lg">Register</span></p>
-      </form>
-   
-     </div>
+  </div>
+      </nav>
+      {active === "login" && <Login/> }
+      {/* <Login /> */}
+      {active === "register" && <Register/>}
+      
     </div>
   )
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,47 @@
+
+export function Login(){
+    return (
+    //   <div className="gif-bg h-screen w-full p-9 text-white text-2xl ">
+        <div className="flex justify-center  items-center text-white">
+  
+        <div className="pop w-[60%] mt-14">
+          <div className="flex justify-center items-center mb-5">
+  
+          <img src="/public/icons8-playerme-100.png" className="w-[100px]" alt=""  />
+          <h1 className="text-7xl font-bold bg-gradient-to-r from-gray-100 via-fuchsia-300 to-yellow-400 text-transparent bg-clip-text pop  text-center">Universe</h1>
+          </div>
+          <p className="pop text-center mb-5">Discover, create, and experience music like never before.</p>
+          <div className="flex gap-1 justify-center items-center">
+          <p className="pop text-center text-fuchsia-300">Discover now</p>
+          <img src="/public/icons8-arrow-right-48 (1).png" alt="" srcset="" />
+  
+          </div>
+         
+        </div>
+       <div className="flex justify-left w-[40%] ">
+  
+        <form className="flex flex-col gap-4 w-[60%] bg-fuchsia-900/80 rounded-4xl mt-18 shadow-2xl border-none p-10
+        backdrop-blur-md bg-gradient-to-r from-indigo-900/80 via-fuchsia-700/50 to-fuchsia-950/80 border border-white/20
+        ">
+          <h1 className="text-4xl font-extrabold block text-center mt-3 mb-6 tracking-wider">Sign<span className="text-fuchsia-100 pop">In</span></h1>
+          
+          <div className="flex w-full mb-4 justify-center">
+          
+          <img src="/public/icons8-user-100 (1).png" className="w-[100px] " alt="" srcset="" />
+  
+          </div>
+          <input type="text" placeholder="Enter your Email" className="p-3 text-[18px] pop rounded-4xl bg-gradient-to-r from-fuchsia-600/40 via-fushsia-400/50 to-indigo-600/30  border border-white/20 backdrop-blur-md shadow-lg  mb-2 bg-fuchsia-800/80 text-white outline-none " />
+          <input type="password" placeholder="Enter your Password" className="text-[18px] pop outline-none p-3  mb-2 bg-fuchsia-800/80 text-white rounded-4xl bg-gradient-to-r from-fuchsia-600/40 via-fushsia-400/50 to-indigo-600/30  border border-white/20 backdrop-blur-md shadow-lg" />
+          <div className="flex w-full justify-center mb-3">
+  
+          <button className="bg-fuchsia-500 bg-gradient-to-r from-indigo-500/80 via-fuchsia-200/40 to-fuchsia-700/70 backdrop-blur-md shadow-lg border border-white/20 hover:scale-[1.1] cursor-pointer duration-300 text-white  pop w-[50%] p-2 rounded-2xl ">Login</button>
+          </div>
+          <p className="text-white text-sm text-center">Don't have an account <span className="text-fuchsia-100 underline cursor-pointer text-lg">Register</span></p>
+        </form>
+        </div>
+  
+     
+       </div>
+      
+    )
+  }
